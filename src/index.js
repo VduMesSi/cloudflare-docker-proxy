28f730172b3594e5c043627b24ec0448d1a4474f
VduMesSi <95524908+VduMesSi@users.noreply.github.com>
2024-11-01 10:22:00 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,18 +6,14 @@ addEventListener("fetch", (event) => {
 const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
-  // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
-
-  // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  "docker.apolenakog.store": "https://registry-1.docker.io",
+  "quay.apolenakog.store": "https://quay.io",
+  "gcr.apolenakog.store": "https://gcr.io",
+  "k8s-gcr.apolenakog.store": "https://k8s.gcr.io",
+  "k8s.apolenakog.store": "https://registry.k8s.io",
+  "ghcr.apolenakog.store": "https://ghcr.io",
+  "cloudsmith.apolenakog.store": "https://docker.cloudsmith.io",
+  "docker-staging.apolenakog.store": dockerHub,
 };
 
 function routeByHosts(host) {
